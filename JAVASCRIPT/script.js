e4884ac99a540892d1da351e6c39535eda567d98
mkrosz <mikewillyf@gmail.com>
2024-04-13 11:55:27 -0300

    Portifólio

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -123,17 +123,26 @@ function openTab(evt, tabName) {
   evt.currentTarget.classList.add("active");
 }
 
-//Aumenta imagem de Certificado
-/*
- // Obtém todas as divs com a classe "options"
- const options = document.querySelectorAll('.options');
+// Aumenta imagem de Certificado
 
- // Itera sobre cada div e adiciona um evento de clique
- options.forEach(option => {
-     option.addEventListener('click', () => {
-         // Obtém a imagem dentro da div clicada
-         const image = option.querySelector('img');
-         // Adiciona uma classe para aumentar o tamanho da imagem
-         image.classList.toggle('enlarged');
-     });
- });*/
+const options = document.querySelectorAll('.options');
+
+options.forEach(option => {
+    option.addEventListener('click', () => {
+        const image = option.querySelector('img');
+        // Remove a classe enlarged para garantir que a imagem não esteja aumentada antes de exibí-la em tela cheia
+        image.classList.remove('enlarged');
+        
+        // Cria uma cópia da imagem
+        const fullscreenImage = image.cloneNode(true);
+        fullscreenImage.classList.add('fullscreen-image');
+        
+        // Adiciona a imagem em tela cheia ao corpo do documento
+        document.body.appendChild(fullscreenImage);
+        
+        // Remove a imagem em tela cheia quando o usuário clicar nela
+        fullscreenImage.addEventListener('click', () => {
+            fullscreenImage.remove();
+        });
+    });
+});
